6faba9f3c1dca39584c4c01d09cae6526d9f2929
tronixwhore <tronixwhore@gmail.com>
2026-04-25 18:22:59 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,5 +1,5 @@
 --[[
-12322222222222222222222233123123123
+
     Milenium Library
     -> Made by @finobe 
     -> Kind of got bored idk what to do with life
@@ -711,7 +711,7 @@
                     Name = "\0";
                     TextColor3 = themes.preset.accent;
                     BorderColor3 = rgb(0, 0, 0);
-                    Text = '<font color="rgb(72, 72, 73)">32 days left, </font>' .. cfg.name .. cfg.suffix;
+                    Text = '<font color="rgb(72, 72, 73)">Free version, </font>' .. cfg.name .. cfg.suffix;
                     Size = dim2(1, 0, 0, 0);
                     Position = dim2(0, -10, 0.5, -1);
                     AnchorPoint = vec2(0, 0.5);
